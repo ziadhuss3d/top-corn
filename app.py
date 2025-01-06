8f264c1f624c062fbc11d909fd3b450b1a25e373
ziadhuss3d <ziadhuss3d@example.com>
2025-01-07 00:37:02 +0200

    s

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template # type: ignore
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 @app.route('/movies')
 def movies():
-    return "This is the movies page!"
+    return render_template('movies.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 if __name__ == "__main__":
     app.run(debug=True)
